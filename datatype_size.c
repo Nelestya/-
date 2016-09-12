56f234d08a01ecd02ebe19982cdef0143decbd53
Dlugosz Tristan <tristand.pro@gmail.com>
2016-09-12 15:54:29 +0200

    ajout de programme explicatif

---
+++ datatype_size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("the 'int' data type is\t\t %d bytes\n", sizeof(int));
+  printf("the 'unsigned int' data type is\t\t %d bytes\n", sizeof(unsigned int));
+  printf("the 'short int' data type is\t\t %d bytes\n", sizeof(short int));
+  printf("the 'long int' data type is\t\t %d bytes\n", sizeof(long int));
+  printf("the 'long long int' data type is\t\t bytes\n", sizeof(long long int));
+  printf("the 'float' data type is\t\t %d bytes\n", sizeof(float));
+  printf("the 'char' data type is\t\t %d bytes\n", sizeof(char));
+}
